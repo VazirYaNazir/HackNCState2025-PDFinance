793d01b3d26ab20f85da0eb9afabb23a7c2dcd29
parsa <parsanemati62@gmail.com>
2025-02-08 13:09:18 -0500

    whoaaaa changing strings into vectors???

--- src/pdfreader.py
+++ src/pdfreader.py
@@ -7,4 +7,13 @@ for page in reader.pages:
     print(page.extract_text())
 
 
-
+def make_vectors(strings):
+    word_dicts = []
+    for s in strings:
+        word_dicts.append(word_count_dictionary(s))
+    word_set = build_clean_set(*word_dicts)
+    vectors = []
+    for d in word_dicts:
+        vectors.append(word_vector(word_set,d))
+    print(word_set)
+    return vectors
